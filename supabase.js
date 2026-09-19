223a05bd411c0c62b0a9c08cd10148fbe57de09c
Js undefined <92572845+701660ks@users.noreply.github.com>
2026-09-19 13:26:27 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+
+const SUPABASE_URL = "https://qfkxnmefqjtsckmkrfld.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_80DMdaZyjRQg9s_dH12W9w_g-q7x3hR";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
