5ec844eddc4525c31fe055798a2e6db9d8f8e992
Js undefined <92572845+701660ks@users.noreply.github.com>
2026-09-19 16:42:29 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,7 @@
 const SUPABASE_URL = "https://qfkxnmefqjtsckmkrfld.supabase.co";
 
+
+
 const SUPABASE_KEY = "sb_publishable_80DMdaZyjRQg9s_dH12W9w_g-q7x3hR";
 
 const supabaseClient = window.supabase.createClient(
