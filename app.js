0f838b63bd39a0990c954fe77fa8b59c9556730c
Js undefined <92572845+701660ks@users.noreply.github.com>
2026-09-20 22:11:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const sb = supabaseClient;
             document.getElementById("loadingScreen");
 
         if (loading) {
+window.alert("kd");
             loading.innerHTML = `
                 <div style="
                     padding:30px;
