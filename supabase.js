fbb12320a437b15fa34221525967ff4ed2e1309a
Js undefined <92572845+701660ks@users.noreply.github.com>
2026-09-20 21:18:38 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -6,7 +6,7 @@ const SUPABASE_KEY = "sb_publishable_80DMdaZyjRQg9s_dH12W9w_g-q7x3hR";
 
     const supabaseClient = window.supabase.createClient(
         SUPABASE_URL,
-        SUPABASE_ANON_KEY
+        SUPABASE_KEY
     );
 
     
