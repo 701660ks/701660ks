225d3924fd3e4d55401a51abcbb68c7d24004417
Js undefined <92572845+701660ks@users.noreply.github.com>
2026-09-20 21:17:54 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -4,15 +4,9 @@ const SUPABASE_URL = "https://qfkxnmefqjtsckmkrfld.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_80DMdaZyjRQg9s_dH12W9w_g-q7x3hR";
 
-
-
-if (!window.supabase) {
-    console.error("Supabase library was not loaded.");
-} else {
-    window.supabaseClient = window.supabase.createClient(
+    const supabaseClient = window.supabase.createClient(
         SUPABASE_URL,
         SUPABASE_ANON_KEY
     );
 
-    console.log("Supabase client initialized successfully.");
-}
+    
